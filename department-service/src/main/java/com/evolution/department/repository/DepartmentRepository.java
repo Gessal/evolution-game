78241d476b159deec07ddf4c40a322e3bf35cc9b
maxim <prince_burunduk@mail.ru>
2021-12-18 14:23:40 +0300

    Initial

---
+++ department-service/src/main/java/com/evolution/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.evolution.department.repository;
+
+import com.evolution.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
---
+++ user-service/src/main/java/com/evolution/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.evolution.user.repository;
+
+import com.evolution.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User,Long> {
+    User findByUserId(Long userId);
+}
